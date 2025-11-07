b20dea478e6b8ab7ec78bcbcae6c80210e5f0c24
German Rios-Lazo <germanrl@byu.edu>
2025-11-07 16:37:10 -0700

    Added metrics to track requests

---
+++ src/metrics.js
@@ -0,0 +1,99 @@
+const { metrics: metricsConfig } = require("./config");
+
+const requestsByEndpoint = {};
+const requestsByMethod = {};
+
+// Middleware to track requests
+function requestTracker(req, res, next) {
+  const endpoint = `[${req.method}] ${req.path}`;
+  requestsByEndpoint[endpoint] = (requestsByEndpoint[endpoint] || 0) + 1;
+  requestsByMethod[req.method] = (requestsByMethod[req.method] || 0) + 1;
+  next();
+}
+
+// Sending metrics every 10 seconds
+setInterval(() => {
+  const metrics = [];
+  Object.keys(requestsByEndpoint).forEach((endpoint) => {
+    metrics.push(
+      createMetric("requestsByEndpoint", requestsByEndpoint[endpoint], "1", "sum", "asInt", {
+        endpoint,
+      })
+    );
+  });
+
+  Object.keys(requestsByMethod).forEach((method) => {
+    metrics.push(
+      createMetric("requestsByMethod", requestsByMethod[method], "1", "sum", "asInt", {
+        method,
+      })
+    );
+  });
+
+  sendMetricToGrafana(metrics);
+}, 10000);
+
+function createMetric(metricName, metricValue, metricUnit, metricType, valueType, attributes) {
+  attributes = { ...attributes, source: metricsConfig.source };
+
+  const metric = {
+    name: metricName,
+    unit: metricUnit,
+    [metricType]: {
+      dataPoints: [
+        {
+          [valueType]: metricValue,
+          timeUnixNano: Date.now() * 1000000,
+          attributes: [],
+        },
+      ],
+    },
+  };
+
+  Object.keys(attributes).forEach((key) => {
+    metric[metricType].dataPoints[0].attributes.push({
+      key: key,
+      value: { stringValue: attributes[key] },
+    });
+  });
+
+  if (metricType === "sum") {
+    metric[metricType].aggregationTemporality = "AGGREGATION_TEMPORALITY_CUMULATIVE";
+    metric[metricType].isMonotonic = true;
+  }
+
+  return metric;
+}
+
+function sendMetricToGrafana(metrics) {
+  const body = {
+    resourceMetrics: [
+      {
+        scopeMetrics: [
+          {
+            metrics,
+          },
+        ],
+      },
+    ],
+  };
+
+  fetch(`${metricsConfig.url}`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      Authorization: `Bearer ${metricsConfig.apiKey}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP status: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.error("Error pushing metrics:", error);
+    });
+}
+
+module.exports = { requestTracker };
